a5151129a04f9cad65c9c2dd2ff40646f8b0c7e3
bestzy <bestzy6@qq.com>
2022-06-04 18:01:24 +0800

    添加基本接口response

--- serializer/common.go
+++ serializer/common.go
@@ -10,6 +10,73 @@ type Response struct {
 	Error string      `json:"error,omitempty"`
 }
 
+// ListResponse 发布列表
+type ListResponse struct {
+	StatusCode int64   `json:"status_code"` // 状态码，0-成功，其他值-失败
+	StatusMsg  *string `json:"status_msg"`  // 返回状态描述
+	VideoList  []Video `json:"video_list"`  // 用户发布的视频列表
+}
+
+// ActionResponse 投稿接口
+type ActionResponse struct {
+	StatusCode int64   `json:"status_code"`
+	StatusMsg  *string `json:"status_msg"`
+}
+
+// UserInfoResponse 用户信息
+type UserInfoResponse struct {
+	StatusCode int64   `json:"status_code"` // 状态码，0-成功，其他值-失败
+	StatusMsg  *string `json:"status_msg"`  // 返回状态描述
+	User       User    `json:"user"`        // 用户信息
+}
+
+// LoginResponse 用户登入
+type LoginResponse struct {
+	StatusCode int64   `json:"status_code"` // 状态码，0-成功，其他值-失败
+	StatusMsg  *string `json:"status_msg"`  // 返回状态描述
+	Token      *string `json:"token"`       // 用户鉴权token
+	UserID     *int64  `json:"user_id"`     // 用户id
+}
+
+// RegisterResponse 用户注册
+type RegisterResponse struct {
+	StatusCode int64  `json:"status_code"` // 状态码，0-成功，其他值-失败
+	StatusMsg  string `json:"status_msg"`  // 返回状态描述
+	Token      string `json:"token"`       // 用户鉴权token
+	UserID     int64  `json:"user_id"`     // 用户id
+}
+
+// FeedResponse 视频流接口
+type FeedResponse struct {
+	NextTime   *int64  `json:"next_time"`   // 本次返回的视频中，发布最早的时间，作为下次请求时的latest_time
+	StatusCode int64   `json:"status_code"` // 状态码，0-成功，其他值-失败
+	StatusMsg  *string `json:"status_msg"`  // 返回状态描述
+	VideoList  []Video `json:"video_list"`  // 视频列表
+}
+
+// Video
+type Video struct {
+	Author        User   `json:"author"`         // 视频作者信息
+	CommentCount  int64  `json:"comment_count"`  // 视频的评论总数
+	CoverURL      string `json:"cover_url"`      // 视频封面地址
+	FavoriteCount int64  `json:"favorite_count"` // 视频的点赞总数
+	ID            int64  `json:"id"`             // 视频唯一标识
+	IsFavorite    bool   `json:"is_favorite"`    // true-已点赞，false-未点赞
+	PlayURL       string `json:"play_url"`       // 视频播放地址
+	Title         string `json:"title"`          // 视频标题
+}
+
+// 视频作者信息
+//
+// User
+type User struct {
+	FollowCount   int64  `json:"follow_count"`   // 关注总数
+	FollowerCount int64  `json:"follower_count"` // 粉丝总数
+	ID            int64  `json:"id"`             // 用户id
+	IsFollow      bool   `json:"is_follow"`      // true-已关注，false-未关注
+	Name          string `json:"name"`           // 用户名称
+}
+
 // TrackedErrorResponse 有追踪信息的错误响应
 type TrackedErrorResponse struct {
 	Response
